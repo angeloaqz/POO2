7dde2e9074f536b7684bb8ba0884182b87be71b5
Angelo Zanatta <angeloantoquirinozanatta@gmail.com>
2026-09-22 20:01:50 -0300

    Add files via upload

---
+++ tbl2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class DivisaoPorZeroException : Exception
+{
+    public DivisaoPorZeroException(string mensagem) : base(mensagem)
+    {
+    }
+}
+
+class Program
+{
+    public static double Dividir(double numero1, double numero2)
+    {
+        if (numero2 == 0)
+        {
+            throw new DivisaoPorZeroException(
+                "Não é possível dividir por zero."
+            );
+        }
+
+        return numero1 / numero2;
+    }
+
+    static void Main()
+    {
+        try
+        {
+            double resultado = Dividir(10, 0);
+
+            Console.WriteLine("Resultado: " + resultado);
+        }
+        catch (DivisaoPorZeroException ex)
+        {
+            Console.WriteLine("Erro: " + ex.Message);
+        }
+    }
+}

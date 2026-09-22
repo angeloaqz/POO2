d83922bfe2c5b8a8dea31a7f1af3527381ad8057
Angelo Zanatta <angeloantoquirinozanatta@gmail.com>
2026-09-22 20:00:35 -0300

    Add files via upload

---
+++ tbl2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+public class UsuarioNaoAutenticadoException : Exception
+{
+    public UsuarioNaoAutenticadoException(string mensagem) : base(mensagem)
+    {
+    }
+}
+
+public class Sistema
+{
+    private bool autenticado = false;
+
+    public void AcessarRecurso()
+    {
+        if (!autenticado)
+        {
+            throw new UsuarioNaoAutenticadoException(
+                "Usuário não autenticado. Faça login para acessar o recurso."
+            );
+        }
+
+        Console.WriteLine("Recurso acessado com sucesso!");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            Sistema sistema = new Sistema();
+
+            sistema.AcessarRecurso();
+        }
+        catch (UsuarioNaoAutenticadoException ex)
+        {
+            Console.WriteLine("Erro: " + ex.Message);
+        }
+    }
+}
